841736847b7c3c87c66c21d2ec347af0ad738aaa
misha <k.postal@yandex.ru>
2019-09-20 23:04:23 +0300

    лаба3

---
+++ лаба3/лаба3.py
@@ -0,0 +1,41 @@
+from graph import *
+
+def draw_built(x,y,a,b,color):
+    penColor(color)
+    brushColor(color)
+    rectangle(x, y, a, b)
+def draw_car(x,y,color):
+    brushColor(color)
+    penColor(color)
+    rectangle(x, y, x+250, y+30)
+    rectangle(x+100,y,x+220,y-25)
+    brushColor("white")
+    penColor("white")
+    rectangle(x+120,y-5,x+150,y-20)
+    rectangle(x+170,y-5,x+200,y-20)
+    brushColor("orange")
+    penColor("yellow")
+    circle(x+60, y+30, 15)
+    circle(x+200, y+30, 15)
+    brushColor("black")
+    penColor("black")
+    rectangle(x+250,y+20,x+259,y+23)
+def draw_car_clone(x,y,color):
+    brushColor(color)
+    penColor(color)
+    rectangle(x, y, x+250, y+30)
+    rectangle(x+100,y,x+220,y-25)
+draw_car_clone(193,445,"orange")
+draw_car(200,450,"yellow")
+draw_built(100,0,160,350,"yellow")
+draw_built(190,0,220,350,"yellow")
+draw_built(250,0,300,350,"yellow")
+draw_built(350,0,440,350,"yellow")
+draw_built(0,0,60,350,"yellow")
+for i in range(71):
+  draw_built(0,5*i,600,5*i+2,"white")
+draw_built(20,140,230,400,(255,255,0))
+for i in range(22):
+  draw_built(20+i*10,140,20+i*10+6,400,"white")
+draw_built(0,350,500,400,(255,255,0))
+run()
