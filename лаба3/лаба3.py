03f11087c02fc714e59a9123ab1c5fc2ee2d44ac
Misha-create <54890643+Misha-create@users.noreply.github.com>
2019-09-23 00:01:20 +0300

    Update лаба3.py

--- лаба3/лаба3.py
+++ лаба3/лаба3.py
@@ -14,19 +14,31 @@ def draw_car(x,y,color):
     rectangle(x+120,y-5,x+150,y-20)
     rectangle(x+170,y-5,x+200,y-20)
     brushColor("orange")
-    penColor("yellow")
+    penColor("orange")
     circle(x+60, y+30, 15)
     circle(x+200, y+30, 15)
-    brushColor("black")
-    penColor("black")
-    rectangle(x+250,y+20,x+259,y+23)
 def draw_car_clone(x,y,color):
     brushColor(color)
     penColor(color)
     rectangle(x, y, x+250, y+30)
     rectangle(x+100,y,x+220,y-25)
-draw_car_clone(193,445,"orange")
-draw_car(200,450,"yellow")
+def fence():
+    brushColor("white")
+    penColor("white")
+    length=5
+    x=0
+    for i in range(34):
+        polygon([(x,400),(x+length,400),(x+3*length,350),(x+2*length,350),(x,400)])
+        x = x + length*3
+def draw_all_car(x,y):
+    draw_car(x-21,y-15,"orange")
+    draw_car(x-14,y-10,(229, 240, 26))
+    draw_car(x-7,y-5,"orange")
+    draw_car(x,y,"yellow")
+    penColor("yellow")
+    brushColor("white")
+    polygon([(x-21+100,y-15),(x-21+100,y-15-25),(x+100,y-25),(x+100,y),(x-21+100,y-15)])
+draw_all_car(200,480)
 draw_built(100,0,160,350,"yellow")
 draw_built(190,0,220,350,"yellow")
 draw_built(250,0,300,350,"yellow")
@@ -38,4 +50,10 @@ draw_built(20,140,230,400,(255,255,0))
 for i in range(22):
   draw_built(20+i*10,140,20+i*10+6,400,"white")
 draw_built(0,350,500,400,(255,255,0))
+draw_built(175,50,425,80,"yellow")
+draw_built(290,80,310,400,"yellow")
+draw_built(0,60,600,70,"yellow")
+draw_built(0,400,600,410,"orange")
+draw_built(0,340,600,350,"orange")
+fence()
 run()
