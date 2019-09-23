a75494cc5c62b1738b9dc4d005580eea42190603
misha <k.postal@yandex.ru>
2019-09-23 08:47:34 +0300

    Update 11(1).py

--- черепашка/11(1).py
+++ черепашка/11(1).py
@@ -1,16 +1,20 @@
-import turtle
-
-turtle.shape('turtle')
-
-def polygon (angle, length):
-    for i in range(angle):
-        turtle.forward(length)
-        turtle.left(360/(360/angle))
-    for i in range(angle):
-        turtle.forward(length)
-        turtle.right(360(360/angle)) 
-
-   
-turtle.left(90)
-for j in range(int(input())):
-    polygon(100+j*25, 60)
+import turtle 
+import math
+tpi=360
+point=1
+turtle.shape("turtle")
+def circle(n,rl):
+    for i in range(math.ceil(tpi*(1+0.1*n))):
+        turtle.forward(1)
+        if rl:
+            turtle.left(1/(1+0.1*n))
+        else:
+            turtle.right(1/(1+0.1*n))
+allcircle=21
+ppi=90
+turtle.left(ppi)
+for i in range(1,allcircle):
+    for j in range(2):
+        circle(i,j)
+    
+    
